a2702ea0c9835b28e049e485fd435aff2e76d88e
amirotul <amirotulkotimah@gmail.com>
2021-07-01 11:37:04 +0700

    TugasIntent

---
+++ TugasIntent/app/src/main/java/com/example/tugasintent/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.tugasintent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        }
+    }
